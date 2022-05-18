f3952a53404acde7c5c08e5e3156ecb67a447710
Vasanti Suthar <63599802+vasantisuthar@users.noreply.github.com>
2022-05-19 01:47:21 +0530

    update index

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const Student = require('./studentSchema');
 const app = express()
 
 console.log("console from master");
+console.log("console from other");
 mongoose.connect('mongodb://localhost:27017/MongoExcelDemo').then(() => {
     console.log('database connected')
 });
